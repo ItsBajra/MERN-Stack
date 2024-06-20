6500dbcc6720332aad9bd171210237ed47eac05e
ItsBajra <buzzraws@gmail.com>
2024-06-20 11:54:27 +0545

    TailwindCSS

---
+++ Class work/Contact-form/script.js
@@ -0,0 +1,9 @@
+function submitForm() {
+    let email = document.getElementById('emaill').value;
+    let subject = document.getElementById('subjectt').value;
+    let description = document.getElementById('desc').value;
+
+    console.log(email)
+    console.log(subject)
+    console.log(description)
+}
