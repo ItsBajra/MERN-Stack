23da8b40f532bd8bde092522ac5577e28fb30d44
ItsBajra <buzzraws@gmail.com>
2024-06-26 10:01:54 +0545

    26 June

---
+++ mongodb/scripts.js
@@ -0,0 +1,20 @@
+const Person = [
+    {
+        fullname: 'Subarna Bajracharya',
+        class: 'L5',
+        age: 21,
+        address: 'Gabahal, Lalitpur'
+    },
+    {
+        fullname: 'Sandip Thapa Magar',
+        class: 'L5',
+        age: 21,
+        address: 'Dholahiti, Lalitpur'
+    },
+    {
+        fullname: 'Aditya Poudel',
+        class: 'L5',
+        age: 21,
+        address: 'Jhapa'
+    }
+]
